3a0b6aabb9e57e9717ffbac8a4a1360402d1a7c3
Jason Pearson <jason.d.pearson@gmail.com>
2020-06-13 23:27:15 -0400

    fix invalid cache and add functionality

--- app/src/main/java/dev/jasonpearson/testing/MainActivity.kt
+++ app/src/main/java/dev/jasonpearson/testing/MainActivity.kt
@@ -2,10 +2,18 @@ package dev.jasonpearson.testing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private val presenter = MainPresenter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.greet_button)?.setOnClickListener {
+            presenter.greet("User")
+        }
     }
 }
